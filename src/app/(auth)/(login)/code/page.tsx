c0836aea03d900c207890c19cf39d3f63d72e45b
Mary <maryamgarousi921@gmail.com>
2024-11-30 09:15:12 +0330

    Update code /page.tsx

--- src/app/(auth)/(login)/code/page.tsx
+++ src/app/(auth)/(login)/code/page.tsx
@@ -3,24 +3,33 @@
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 
-export default function ForgotPassword() {
-  const [username, setUsername] = useState<string>("");
+export default function Code() {
+  const [mobile, setMobile] = useState<string>("");
+  const [otp, setOtp] = useState<string[]>(Array(5).fill(""));
   const router = useRouter();
 
-  const handleSendCode = (e: React.FormEvent) => {
+  const handleLogin = (e: React.FormEvent) => {
     e.preventDefault();
-    if (username.trim() === "") {
-      alert("لطفاً شماره موبایل خود را وارد کنید.");
-      return;
-    }
+    console.log("mobile:", mobile);
+    console.log("OTP:", otp.join(""));
 
-    console.log("Username:", username);
-    router.push("/code");
+    router.push("/resetPassword");
+  };
+
+  const handleOtpChange = (value: string, index: number) => {
+    const updatedOtp = [...otp];
+    updatedOtp[index] = value.slice(0, 1);
+    setOtp(updatedOtp);
+
+    const nextField = document.getElementById(`otp-${index + 1}`);
+    if (nextField && value) {
+      (nextField as HTMLInputElement).focus();
+    }
   };
 
   return (
-    <div className="w-[500px] mt-10 p-6 bg-re rounded-lg">
-      <div className="flex flex-col items-center mb-16">
+    <div className="w-full max-w-md mx-auto mt-10 p-6 bg-white rounded-lg">
+      <div className="flex flex-col items-center mb-24">
         <img
           src="/icons/Group 31.svg"
           alt="Tree News"
@@ -31,24 +40,40 @@ export default function ForgotPassword() {
         <h2 className="text-xl font-bold text-gray-800">Tree news</h2>
       </div>
 
-      <form onSubmit={handleSendCode}>
+      <form onSubmit={handleLogin}>
         <div>
-          <label htmlFor="username" className="block text-right text-gray-700">
+          <label htmlFor="username" className="block text-right text-gray-700 ">
             موبایل
           </label>
           <input
             type="text"
             id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
+            value={mobile}
+            onChange={(e) => setMobile(e.target.value)}
             className="w-full px-4 py-3 border border-gray-300 rounded-[15px] focus:outline-none focus:ring-2 focus:ring-red-500 mt-2"
+            placeholder="مثال: 746****0937"
           />
         </div>
+
+        <div className="flex justify-between mt-10 mb-2">
+          {otp.map((digit, index) => (
+            <input
+              key={index}
+              type="text"
+              id={`otp-${index}`}
+              value={digit}
+              onChange={(e) => handleOtpChange(e.target.value, index)}
+              className="w-14 h-14 text-center border border-gray-300 rounded-[15px] focus:outline-none focus:ring-2 focus:ring-red-500 text-lg"
+              maxLength={1}
+            />
+          ))}
+        </div>
+
         <button
           type="submit"
-          className="w-full py-2 text-white bg-[#AC2043] transition rounded-[15px] mb-6 mt-12"
+          className="w-full py-3 text-white bg-[#AC2043] transition rounded-[15px] mb-6 mt-12"
         >
-          ارسال کد
+          بازیابی رمز عبور
         </button>
       </form>
     </div>
