df12a55828638fb5bad96e33c7a3b97c1821abcf
Mary-grs <maryamgarousi921@gmail.com>
2024-12-11 16:14:18 +0330

    edit

--- src/app/panel/dashboard/page.tsx
+++ src/app/panel/dashboard/page.tsx
@@ -71,7 +71,7 @@ const Dashboard = () => {
 
         <div className="overflow-x-auto mx-4 mt-4 ">
           <div className="w-full bg-white shadow-md overflow-hidden">
-            <div className="bg-[#AC2043] text-white rounded-[10px] w-[1103px] flex">
+            <div className="bg-[#AC2043] text-white rounded-[10px] w-full flex">
               <p className="py-3 px-4 text-right">ردیف</p>
               <p className="py-3 px-24 text-right mr-[100px]">عنوان خبر</p>
               <p className="py-3 px-4 text-right mr-[100px]">دسته بندی</p>
